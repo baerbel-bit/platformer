7157a883742dd037291ebf8a941e430f8d09e285
baerbel <leo.georgi@proton.me>
2026-06-21 21:06:59 +0200

    added jumb mecanic

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 import pygame
-import urllib.request
+import numpy
+
 pygame.init()
 pygame.display.set_caption("first game platformer")
-win_surface = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+win_surface = pygame.display.set_mode((500, 500), pygame.RESIZABLE)
 win_size = win_surface.get_size()
 
-#fetching,loading and scailing and  the background
-url = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%2Fid%2FOIP.wGREfI0D3BMQRjAy374zGgHaE0%3Fpid%3DApi&f=1&ipt=139b75c5e5219669308854aa30049220b5a4ae832dd0bd5c2857f4f455904711&ipo=images"
-urllib.request.urlretrieve(url, "hintergrund.png")
-hintergrund = pygame.image.load("hintergrund.png")
-hintergrund = pygame.transform.scale(hintergrund, win_size)
+hintergrund = pygame.Surface(win_size)
+hintergrund.fill((0, 255, 0))
 
 
-
+# variablen for player size
 x = 50
 y = 50
 width = 40
 height = 60
 vel = 5
+vel1 = 10
+# body to jump on rectancle
 
+x1 = 100
+y1 = 100
+width1 = 100
+height1 = 100
 
 #main game loob
 run = True
@@ -28,19 +32,30 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        if event.type == pygame.VIDEORESIZE:
+            win_size = event.size
+            win_surface = pygame.display.set_mode(win_size, pygame.RESIZABLE)
+            hintergrund = pygame.Surface(win_size)
+            hintergrund.fill((0, 255, 0))
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_v:
+                y -= vel1
 
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
+    if keys[pygame.K_a]:
         x -= vel
-    if keys[pygame.K_RIGHT]:
+    if keys[pygame.K_d]:
         x += vel
-    if keys[pygame.K_UP]:
+    if keys[pygame.K_w]:
         y -= vel
-    if keys[pygame.K_DOWN]:
+    if keys[pygame.K_s]:
         y += vel
+
+
 
     win_surface.blit(hintergrund, (0, 0))
     pygame.draw.rect(win_surface, (255, 0, 0), (x, y, width, height))
+    pygame.draw.rect(win_surface, (255, 0, 0), (x1, y1, width1, height1))
     pygame.display.update()
 
 pygame.quit()
